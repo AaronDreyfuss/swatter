1933d8e7921708c5564d3e4ab96ccbdea1863bd0
AaronDreyfuss <AaronDreyfuss@gmail.com>
2026-05-23 21:48:06 -0400

    Add Landing page with login and register links

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,5 +1,14 @@
+import { Link } from 'react-router-dom';
+
 function Landing() {
-  return <div>Landing</div>;
+  return (
+    <div>
+      <h1>Swatter</h1>
+      <p>Track bugs, manage projects, and collaborate with your team.</p>
+      <Link to="/login">Log in</Link>
+      <Link to="/register">Register</Link>
+    </div>
+  );
 }
 
 export default Landing;
